4b90ea3be5586e5ffbd3de1c1ef9c623a52eab20
Anton <info@yammycode.com>
2022-10-21 21:40:03 +0300

    Initial Commit

---
+++ ColorPickerSwiftUI/ColorPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerSwiftUIApp.swift
+//  ColorPickerSwiftUI
+//
+//  Created by Anton Saltykov on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
